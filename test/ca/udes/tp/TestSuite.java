1d00bd0a14fecfe00fee35bdb91004ff7f47de5f
migm3002 <maick.migeon--le.ligne@usherbrooke.ca>
2017-11-30 18:37:42 -0500

    changed package for test suite

--- test/ca/udes/tp/TestSuite.java
+++ test/ca/udes/tp/TestSuite.java
@@ -1,8 +1,10 @@
-package ca.udes.tp.object;
+package ca.udes.tp;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
+
+import ca.udes.tp.object.MatchTest;
 
 @RunWith(Suite.class)
 @SuiteClasses(
