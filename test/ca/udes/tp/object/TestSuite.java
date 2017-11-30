f48fb4cbd7f2e2ec472a7e94d25013226db41a9c
migm3002 <maick.migeon--le.ligne@usherbrooke.ca>
2017-11-30 18:33:24 -0500

    Test suite created

---
+++ test/ca/udes/tp/object/TestSuite.java
@@ -0,0 +1,15 @@
+package ca.udes.tp.object;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses(
+		{
+			MatchTest.class
+		}
+	)
+public class TestSuite {
+
+}
