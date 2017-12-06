0f9f7efbb744fb66e9984229a7f8542478fc4fb9
mdja38 <mdjahel@gmail.com>
2017-12-06 15:09:24 -0500

    "des trucs inutiles ont été enlevés"

--- test/integrationTests/TestIntegrationPariBDD.java
+++ test/integrationTests/TestIntegrationPariBDD.java
@@ -9,39 +9,13 @@ import org.junit.Before;
 import org.junit.Test;
 
 import ca.udes.tp.dao.PariDAO;
-import ca.udes.tp.object.Joueur;
-import ca.udes.tp.object.Match;
-import ca.udes.tp.object.Pari;
-import ca.udes.tp.object.Pari.Status;
-import ca.udes.tp.object.Score;
+
 
 public class TestIntegrationPariBDD {
-	private Pari pariTest;
-	private Match matchTest;
-	private Match matchTest2;
-	private Pari pariMatch2_1;
-	private Pari pariMatch2_2;
 
 	@Before
 	public void setUp(){
 		//Ajouter script de BDD pour la simuler
-		Joueur j1 = new Joueur(1, "NADAL", "Rafael", 1, true);
-		Joueur j2 = new Joueur(2, "FEDERER", "Roger", 2, false);
-		Joueur j3 = new Joueur(3, "WILLIAMS", "Serena", 1, false);
-		Joueur j4 = new Joueur(4, "SHARAPOVA", "Maria", 2, false);
-		
-		String[][] tabSet1 = {{"3","",""},{"5","",""}};
-		String[] tabJeu1 = {"0","30"};
-		Score s1 = new Score(tabSet1, tabJeu1);
-		
-		String[][] tabSet3 = {{"6","2","6"},{"4","6","3"}};
-		String[] tabJeu3 = {"",""};
-		Score s3 = new Score(tabSet3, tabJeu3);
-		
-		matchTest = new Match(1, j1, j2, s1, "0:32:00", 3, 3);
-		matchTest2 = new Match(3, j3, j4, s3, "2:06:51", 3, 3);
-		pariMatch2_1 = new Pari(3, 3, 3, 51.86 , Status.commited);
-		pariMatch2_2 = new Pari(4, 3, 4, 22.45 , Status.commited);
 	}
 
 	@After
