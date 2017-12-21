41cb478dcf5e681aca372356eddff18711a2b410
mdja38 <mdjahel@gmail.com>
2017-12-20 20:31:12 -0500

    "add test integrat gen event + matchbdd and new functions in testutil"

--- test/utility/TestUtility.java
+++ test/utility/TestUtility.java
@@ -2,7 +2,9 @@ package utility;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import ca.udes.tp.dao.ConnectionDB;
 
@@ -86,7 +88,72 @@ public final class TestUtility {
 			"INSERT INTO Rencontre(idMatch, vainqueur)\r\n" + 
 			"VALUES(1,null);";
 	
+	private static final String SCRIPT_FOR_INTEGRATION_MATCH_WINNER = "INSERT INTO Joueur(idJoueur, nom, prenom, classement)\r\n" + 
+			"VALUES(1, 'Nadal', 'Rafael', 1);\r\n" + 
+			"INSERT INTO Joueur(idJoueur, nom, prenom, classement)\r\n" + 
+			"VALUES(3, 'Federer', 'Roger', 2);\r\n" + 
+			"INSERT INTO Joueur(idJoueur, nom, prenom, classement)\r\n" + 
+			"VALUES(5, 'Murray', 'Andy', 3);\r\n" + 
+			"INSERT INTO Joueur(idJoueur, nom, prenom, classement)\r\n" + 
+			"VALUES(7, 'Djokovic', 'Novak', 6);\r\n" + 
+			"INSERT INTO Joueur(idJoueur, nom, prenom, classement)\r\n" + 
+			"VALUES(9, 'Wavrinka', 'Stan', 8);\r\n" + 
+			"INSERT INTO Joueur(idJoueur, nom, prenom, classement)\r\n" + 
+			"VALUES(10, 'Thiem', 'Dominic', 7);\r\n" + 
+			"INSERT INTO Joueur(idJoueur, nom, prenom, classement)\r\n" + 
+			"VALUES(8, 'Raonic', 'Milos', 12);\r\n" + 
+			"INSERT INTO Joueur(idJoueur, nom, prenom, classement)\r\n" + 
+			"VALUES(6, 'Monfils','Gael', 42);\r\n" + 
+			"INSERT INTO Joueur(idJoueur, nom, prenom, classement)\r\n" + 
+			"VALUES(4, 'Tsonga', 'Jo Wilfried', 18);\r\n" + 
+			"INSERT INTO Joueur(idJoueur, nom, prenom, classement)\r\n" + 
+			"VALUES(2, 'Nishikori', 'Kei', 14);\r\n" + 
+			"INSERT INTO Joueur(idJoueur, nom, prenom, classement)\r\n" + 
+			"VALUES(11, 'Williams', 'Serena', 24);\r\n" + 
+			"INSERT INTO Joueur(idJoueur, nom, prenom, classement)\r\n" + 
+			"VALUES(13, 'Muguruza', 'Garbine', 2);\r\n" + 
+			"INSERT INTO Joueur(idJoueur, nom, prenom, classement)\r\n" + 
+			"VALUES(15, 'Sharapova', 'Maria', 86);\r\n" + 
+			"INSERT INTO Joueur(idJoueur, nom, prenom, classement)\r\n" + 
+			"VALUES(17, 'Halep', 'Simona', 1);\r\n" + 
+			"INSERT INTO Joueur(idJoueur, nom, prenom, classement)\r\n" + 
+			"VALUES(19, 'Bouchard', 'Eugenie', 78);\r\n" + 
+			"INSERT INTO Joueur(idJoueur, nom, prenom, classement)\r\n" + 
+			"VALUES(20, 'Mladenovic', 'Kristina', 13);\r\n" + 
+			"INSERT INTO Joueur(idJoueur, nom, prenom, classement)\r\n" + 
+			"VALUES(18, 'Garcia', 'Caroline', 9);\r\n" + 
+			"INSERT INTO Joueur(idJoueur, nom, prenom, classement)\r\n" + 
+			"VALUES(16, 'Pliskova', 'Karolina', 3);\r\n" + 
+			"INSERT INTO Joueur(idJoueur, nom, prenom, classement)\r\n" + 
+			"VALUES(14, 'Kuznetsova', 'Svetlana', 8);\r\n" + 
+			"INSERT INTO Joueur(idJoueur, nom, prenom, classement)\r\n" + 
+			"VALUES(12, 'Kvitova', 'Petra', 17);\r\n" +  
+			"INSERT INTO Rencontre(idMatch, vainqueur)\r\n" + 
+			"VALUES(1,null);\r\n" + 
+			"INSERT INTO Rencontre(idMatch, vainqueur)\r\n" + 
+			"VALUES(2,null);\r\n" + 
+			"INSERT INTO Rencontre(idMatch, vainqueur)\r\n" + 
+			"VALUES(3,null);\r\n" + 
+			"INSERT INTO Rencontre(idMatch, vainqueur)\r\n" + 
+			"VALUES(4,null);\r\n" + 
+			"INSERT INTO Rencontre(idMatch, vainqueur)\r\n" + 
+			"VALUES(5,null);\r\n" + 
+			"INSERT INTO Rencontre(idMatch, vainqueur)\r\n" + 
+			"VALUES(6,null);\r\n" + 
+			"INSERT INTO Rencontre(idMatch, vainqueur)\r\n" + 
+			"VALUES(7,13);\r\n" + 
+			"INSERT INTO Rencontre(idMatch, vainqueur)\r\n" + 
+			"VALUES(8,null);\r\n" + 
+			"INSERT INTO Rencontre(idMatch, vainqueur)\r\n" + 
+			"VALUES(9,null);\r\n" + 
+			"INSERT INTO Rencontre(idMatch, vainqueur)\r\n" + 
+			"VALUES(10,null);\r\n";
+	
 	//private static final String SCRIPT_ADD_DB_MANAGER = "INSERT"
+	
+	private static final String SCRIPT_SELECT_MATCH_WINNER = "SELECT idMatch "
+			+ "FROM public.Rencontre r "
+			+ "WHERE r.vainqueur IS NOT NULL ";
 	
 	public static void CleanUpDB() {
 
@@ -155,6 +222,69 @@ public final class TestUtility {
 				}
 			}
 		}
+	}
+	
+	public static void addInfosForTestIntegrationMatch() {
+
+		PreparedStatement cleanPs = null;
+
+		Connection connection = ConnectionDB.connection();
+		if(connection != null) {
+			try {
+				cleanPs = connection.prepareStatement(SCRIPT_FOR_INTEGRATION_MATCH_WINNER);
+				cleanPs.executeUpdate();
+			}catch(SQLException e) {
+				e.printStackTrace();
+			}finally {
+				if(cleanPs!=null) {
+					try {
+						cleanPs.close();
+					}catch(SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+	}
+	
+	public static int[] obtainMatchWithWinners() {
+		int[] idsMatchs = new int[10];
+
+		PreparedStatement getidsMatchPrepStmt = null;
+		ResultSet rsIdsMatchs = null;
+
+		Connection connection = ConnectionDB.connection();
+
+		if(connection != null) {
+			try {
+				getidsMatchPrepStmt = connection.prepareStatement(SCRIPT_SELECT_MATCH_WINNER, ResultSet.TYPE_SCROLL_SENSITIVE, 
+                        ResultSet.CONCUR_UPDATABLE);
+				rsIdsMatchs = getidsMatchPrepStmt.executeQuery();
+				int sizeRS = -1;
+				
+				if (rsIdsMatchs.last()) {
+					  sizeRS = rsIdsMatchs.getRow();
+					  rsIdsMatchs.beforeFirst();
+				}
+
+				while(rsIdsMatchs.next()) {
+					for(int i=0; i<sizeRS+1; i++) {
+						idsMatchs[0] = (rsIdsMatchs.getInt(1));
+					}
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} finally {
+				ArrayList<PreparedStatement> psArray = new ArrayList<PreparedStatement>();
+				ArrayList<ResultSet> rsArray = new ArrayList<ResultSet>();
+
+				psArray.add(getidsMatchPrepStmt);
+				rsArray.add(rsIdsMatchs);
+
+				ConnectionDB.closeConnections(rsArray, psArray, connection);
+			}
+		}
+		return idsMatchs;
 	}
 	
 	/*public static void addInfosForTestDatabaseManager() {
